ed8635b1d987c96daf1bd37623261d5d0dcc7990
Administrator <Administrator@TPX113JSNHNLNOD>
2018-07-05 00:43:48 +0700

    second commit

--- main.js
+++ main.js
@@ -1,32 +1,59 @@
 var fs = require("fs");
 var http = require("http");
 var url = require("url");
-var server = http.createServer();
-server.listen(8888);
-server.on('request', function (request, response) {
-    debugger;
-    var pathname = url.parse(request.url).pathname;
-    //plugin
-    if (pathname === "/plugin/jquery.min.js") {
-        var txt = fs.readFileSync("plugin/jquery.min.js", "utf8");
-        response.write(txt);
+var crypto = require('crypto');
+
+var server = function () {
+    var server = http.createServer();
+    server.listen(8888);
+    server.on('request', function (request, response) {
+        debugger;
+        var pathname = url.parse(request.url).pathname;
+        //plugin
+        if (pathname === "/plugin/jquery.min.js") {
+            var txt = fs.readFileSync("plugin/jquery.min.js", "utf8");
+            response.write(txt);
+            response.end();
+            return;
+        }
+        //js
+        if (pathname === "/js/app.js") {
+            var txt = fs.readFileSync("js/app.js", "utf8");
+            response.write(txt);
+            response.end();
+            return;
+        }
+        //html
+        if (pathname === "/") {
+            var txt = fs.readFileSync("html/app.html", "utf8");
+            response.write(txt);
+            response.end();
+            return;
+        }
+        if (pathname === "/genRsaKey") {
+            let body = [];
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            }).on('end', () => {
+                body = Buffer.concat(body).toString();
+                var data =JSON.parse(body)
+                savePassword(data);
+            });
+        }
+        response.write("Error 404");
         response.end();
-        return;
+    });
+    function hashPassword(password) {
+        var salt = crypto.randomBytes(128).toString('base64');
+        var iterations = 10000;
+        var hash = crypto.pbkdf2(password, salt, iterations);
+
+        return {
+            salt: salt,
+            hash: hash        };
     }
-    //js
-    if (pathname === "/js/app.js") {
-        var txt = fs.readFileSync("js/app.js", "utf8");
-        response.write(txt);
-        response.end();
-        return;
+    function savePassword(passData) {
+        var stringData = JSON.stringify(passData);
+        fs.writeFileSync("Storage/password.txt", stringData);
     }
-    //html
-    if (pathname === "/") {
-        var txt = fs.readFileSync("html/app.html", "utf8");
-        response.write(txt);
-        response.end();
-        return;
-    }
-    response.write("Error 404");
-    response.end();
-});
+}();
