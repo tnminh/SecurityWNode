082026f883fe5c8eb9542b008c7499db6cdf3617
Administrator <Administrator@TPX113JSNHNLNOD>
2018-07-06 07:52:28 +0700

    4 commit

--- main.js
+++ main.js
@@ -7,64 +7,80 @@ var KEYPAIR = require('keypair');
 
 
 var server = function () {
+    var STORAGELOCATION = 'Storage/data.txt';
     var server = http.createServer();
     server.listen(8888);
     server.on('request', function (request, response) {
-        debugger;
-        var pathname = url.parse(request.url).pathname;
-        //plugin
-        if (pathname.includes("/resources/")) {
-            var txt = fs.readFileSync(pathname, "utf8");
-            response.write(txt);
-            response.end();
-            return;
-        }
-        if (pathname === "/plugin/jquery.min.js") {
-            var txt = fs.readFileSync("plugin/jquery.min.js", "utf8");
-            response.write(txt);
-            response.end();
-            return;
-        }
-        //js
-        if (pathname === "/js/app.js") {
-            var txt = fs.readFileSync("js/app.js", "utf8");
-            response.write(txt);
-            response.end();
-            return;
-        }
-        //html
-        if (pathname === "/") {
-            var txt = fs.readFileSync("html/app.html", "utf8");
-            response.write(txt);
-            response.end();
-            return;
-        }
-        if (pathname === "/saveData") {
-            let body = [];
-            request.on('data', (chunk) => {
-                body.push(chunk);
-            }).on('end', () => {
-                body = Buffer.concat(body).toString();
-                var data = JSON.parse(body);
-                var password = hashPassword(data.kData.passphrase);
-                var rsa = new RSA();
-                var rsaKey = rsa.genRsaKey(data.kData.keylen).getKey();
-//                var dataSave = {password: password, rsaKey: rsaKey, pData: data.pData};
-                saveData(data.pData, "data");
-                saveData(rsaKey, "key");
-                saveData(password, "password");
+        try {
+            debugger;
+            var pathname = url.parse(request.url).pathname;
+            //plugin
+            if (pathname.includes("/resources/")) {
+                var txt = fs.readFileSync(pathname.substring(1), "utf8");
+                response.write(txt);
+                response.end();
+                return;
+            }
+             //html
+             if (pathname === "/") {
+                var txt = fs.readFileSync("html/app.html", "utf8");
+                response.write(txt);
+                response.end();
+                return;
+            }
+
+            if (pathname === "/loadData") {
+                let body = [];
+                request.on('data', (chunk) => {
+                    body.push(chunk);
+                }).on('end', () => {
+                    var email = Buffer.concat(body).toString();
+                    var data=loadData();
+                    data=JSON.parse(data)
+                    var emailData=data.filter(function(rec){
+                        return rec.email===email;
+                    });
+                    emailData=emailData[0];
+                    response.write(JSON.stringify({email:emailData.email,name:emailData.name,birthday:emailData.birthday,phone:emailData.phone,address:emailData.address}));
+                    response.end();
+                    return;
+                });
+            }
+            if (pathname === "/saveData") {
+                let body = [];
+                request.on('data', (chunk) => {
+                    body.push(chunk);
+                }).on('end', () => {
+                    body = Buffer.concat(body).toString();
+                    var data = JSON.parse(body);
+                    var password = hashPassword(data.kData.passphrase);
+                    var rsa = new RSA();
+                    var rsaKey = rsa.genRsaKey(data.kData.keylen).getKey();
+                    //                var dataSave = {password: password, rsaKey: rsaKey, pData: data.pData};
+                    data.pData.password = password;
+                    oldData=JSON.parse(loadData());
+                    saveData(data.pData, STORAGELOCATION);
+                    saveData({ 'key': rsaKey.public }, data.kData.fileLocation + 'public.txt');
+                    saveData({ 'key': rsaKey.public }, data.kData.fileLocation + 'private.txt');
+
+                    // saveData(password, "password");
 
 
-            });
+                });
+            }
         }
-        response.write("Error 404");
-        response.end();
+        catch (err) {
+            console.log(err);
+            response.write("Error");
+            response.end();
+        }
+
     });
     function hashPassword(password) {
         function generateSalt(len) {
             var set = '0123456789abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ',
-                    setLen = set.length,
-                    salt = '';
+                setLen = set.length,
+                salt = '';
             for (var i = 0; i < len; i++) {
                 var p = Math.floor(Math.random() * setLen);
                 salt += set[p];
@@ -75,7 +91,8 @@ var server = function () {
         var hash = crypto.createHash('md5').update(salt + password.toString()).digest('hex');
         return {
             salt: salt,
-            hash: hash};
+            hash: hash
+        };
     }
     function checkPassword(password) {
         var data = JSON.parse(fs.writeFileSync("Storage/password.txt", 'utf8'));
@@ -83,8 +100,25 @@ var server = function () {
         return hash === data.hash;
     }
     function saveData(data, fileName) {
-        var stringData = JSON.stringify(data);
-        fs.writeFileSync("Storage/" + fileName + ".txt", stringData);
+        try {
+            var stringData = JSON.stringify(data);
+            fs.writeFileSync(fileName, stringData, { flag: 'w' });
+        }
+        catch (err) {
+            return "Đường dẫn không hợp lệ"
+        }
+        return 'ok';
+
+    }
+    function loadData() {
+        try {
+            var txt = fs.readFileSync(STORAGELOCATION, "utf8");
+            return txt;
+        }
+        catch (err) {
+            return 'error';
+        }
+
     }
 }();
 var RSA = function () {
@@ -97,5 +131,5 @@ var RSA = function () {
     function getKey() {
         return keypair;
     }
-    return {genRsaKey: genRsaKey, getKey: getKey};
+    return { genRsaKey: genRsaKey, getKey: getKey };
 };
--- resources/js/app.js
+++ resources/js/app.js
@@ -10,24 +10,26 @@ var crypto = function () {
     $("#btn_save").click(function () {
         genRsaKey();
     });
-    function genRsaKey() {
+    function createData() {
         var keylen = $("#txt_key_len").val();
         var passphrase = $("#txt_passphrase").val();
-        var email=$("#txt_email").val();
-        var name=$("#txt_name").val();
-        var birthday=$("#txt_dob").val();
-        var phone=$("#txt_phone").val();
-        var adr=$("#txt_adr").val();
-        var pData={
-            email:email,
-            name:name,
-            birthday:birthday,
-            phone:phone,
-            adr:adr};
-        var kData={
-            keylen:keylen,
-            passphrase:passphrase};
-        var data=JSON.stringify({pData:pData,kData:kData});
+        var email = $("#txt_email").val();
+        var name = $("#txt_name").val();
+        var birthday = $("#txt_dob").val();
+        var phone = $("#txt_phone").val();
+        var adr = $("#txt_adr").val();
+        var pData = {
+            email: email,
+            name: name,
+            birthday: birthday,
+            phone: phone,
+            adr: adr
+        };
+        var kData = {
+            keylen: keylen,
+            passphrase: passphrase
+        };
+        var data = JSON.stringify({ pData: pData, kData: kData });
         $.ajax({
             url: "/saveData",
             type: "POST",
@@ -40,4 +42,185 @@ var crypto = function () {
             }
         });
     }
+
+}();
+var myForm = function () {
+    function loadDataFromEmail(email) {
+        $.ajax({
+            url: "/loadData",
+            type: "POST",
+            data: email,
+            success: function (response) {
+                response= JSON.parse(response);
+                appendCreatingForm(response.email,response.name,response.birthday,response.phone,response.address);
+            },
+            error: function () {
+                alert("Gen RSA Key error");
+            }
+        });
+    }
+    function appendBeginForm(email = "") {
+        if (typeof w2ui['form'] !== "undefined") {
+            w2ui['form'].destroy();
+        }
+        $('#form_create').w2form({
+            name: 'form',
+            header: 'Thông tin',
+            fields: [
+                { field: 'email', type: 'email', required: true, html: { caption: 'Email', attr: 'style="width: 300px"' } },
+
+            ],
+            actions: {
+                'Create': function (event) {
+                    if (w2ui['form'].validate().length > 0) {
+                        return;
+                    }
+                    appendCreatingForm(w2ui['form'].record['email']);
+                },
+                'Load': function (event) {
+                    if (w2ui['form'].validate().length > 0) {
+                        return;
+                    }
+                    loadDataFromEmail(w2ui['form'].record['email']);
+                },
+                'Clear': function (event) {
+                    console.log('clear', event);
+                    this.clear();
+                }
+
+
+            }
+        });
+        w2ui['form'].record['email'] = email;
+        w2ui['form'].refresh();
+    };
+    function appendCreatingForm(email = "") {
+        $(function () {
+            w2ui['form'].destroy();
+            $('#form_create').w2form({
+                name: 'form',
+                header: 'Thông tin',
+                fields: [
+                    { field: 'email', type: 'email', required: true, html: { caption: 'Email', attr: 'style="width: 300px"' } },
+                    { field: 'name', type: 'text', required: true, html: { caption: 'Họ Tên', attr: 'style="width: 300px"' } },
+                    { field: 'birthday', type: 'date', required: true, html: { caption: 'Ngày Sinh', attr: 'style="width: 300px"' } },
+                    { field: 'phone', type: 'number', required: true, html: { caption: 'SĐT', attr: 'style="width: 300px"' } },
+                    { field: 'address', type: 'textarea', required: true, html: { caption: 'Địa Chỉ', attr: 'style="width: 300px; height: 90px"' } },
+                    { field: 'keyLen', type: 'number', required: true, html: { caption: 'Độ dài key', attr: 'style="width: 300px"' } },
+                    { field: 'passphrase', type: 'password', required: true, html: { caption: 'Mật Khẩu', attr: 'style="width: 300px"' } },
+                    { field: 'fileLocation', type: 'textarea', required: true, html: { caption: 'Đường Dẫn Export', attr: 'style="width: 300px"' } },
+
+
+                ],
+                actions: {
+                    'Create': function (event) {
+                        createData()
+                    },
+                    'Clear': function (event) {
+                        console.log('clear', event);
+                        this.clear();
+                    },
+                    'Back': function (event) {
+                        appendBeginForm(w2ui['form'].record['email']);
+                    },
+
+                }
+            });
+            w2ui['form'].record['email'] = email;
+            w2ui['form'].refresh();
+
+        });
+    }
+    function appendLoadingingForm(email ,name,birthday,phone,address) {
+        $(function () {
+            w2ui['form'].destroy();
+            $('#form_create').w2form({
+                name: 'form',
+                header: 'Thông tin',
+                fields: [
+                    { field: 'email', type: 'email', required: true, html: { caption: 'Email', attr: 'style="width: 300px"' } },
+                    { field: 'name', type: 'text', required: true, html: { caption: 'Họ Tên', attr: 'style="width: 300px"' } },
+                    { field: 'birthday', type: 'date', required: true, html: { caption: 'Ngày Sinh', attr: 'style="width: 300px"' } },
+                    { field: 'phone', type: 'number', required: true, html: { caption: 'SĐT', attr: 'style="width: 300px"' } },
+                    { field: 'address', type: 'textarea', required: true, html: { caption: 'Địa Chỉ', attr: 'style="width: 300px; height: 90px"' } }
+
+                ],
+                actions: {
+                    'Save': function (event) {
+                        console.log('save', event);
+                    },
+                    'Clear': function (event) {
+                        console.log('clear', event);
+                        this.clear();
+                    },
+                    'Back': function (event) {
+                        appendBeginForm(w2ui['form'].record['email']);
+                    },
+
+                }
+            });
+            w2ui['form'].record['email'] = email;
+            w2ui['form'].record['name'] = name;
+            w2ui['form'].record['birthday'] = birthday;
+            w2ui['form'].record['phone'] = phone;
+            w2ui['form'].record['address'] = address;
+            
+            w2ui['form'].refresh();
+
+        });
+    }
+    function createData() {
+        if (w2ui['form'].validate().length > 0) {
+            return;
+        }
+        var keylen = w2ui['form'].get('keyLen').el.value;
+        if (keylen % 64 !== 0) {
+            w2popup.open({
+                title: 'Lỗi',
+                body: '<div class="w2ui-centered">Độ dài key phải chia hết cho 64</div>'
+            });
+            return;
+        }
+        if (keylen < 512 || keylen > 1024) {
+            w2popup.open({
+                title: 'Lỗi',
+                body: '<div class="w2ui-centered">Độ dai key phải từ (512-1024)</div>'
+            });
+            return;
+        }
+        debugger;
+        var passphrase = w2ui['form'].get('passphrase').el.value;
+        var email = w2ui['form'].get('email').el.value;
+        var name = w2ui['form'].get('name').el.value;
+        var birthday = w2ui['form'].get('birthday').el.value;
+        var phone = w2ui['form'].get('phone').el.value;
+        var adr = w2ui['form'].get('address').el.value;
+        var fileLocation = w2ui['form'].get('fileLocation').el.value;
+
+        var pData = {
+            email: email,
+            name: name,
+            birthday: birthday,
+            phone: phone,
+            adr: adr
+        };
+        var kData = {
+            keylen: keylen,
+            passphrase: passphrase,
+            fileLocation: fileLocation
+        };
+        var data = JSON.stringify({ pData: pData, kData: kData });
+        $.ajax({
+            url: "/saveData",
+            type: "POST",
+            data: data,
+            success: function (response) {
+                alert("Gen RSA Key success");
+            },
+            error: function () {
+                alert("Gen RSA Key error");
+            }
+        });
+    }
+    appendBeginForm();
 }();
